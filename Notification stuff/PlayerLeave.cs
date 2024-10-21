c88cd051b4aac96ef33b99553c3a665aebd48763
N*vuh <90987807+Novuh-Kaen@users.noreply.github.com>
2024-10-21 03:40:26 -0400

    Add files via upload

---
+++ Notification stuff/PlayerLeave.cs
@@ -0,0 +1,23 @@
+﻿using HarmonyLib;
+using Photon.Pun;
+using Photon.Realtime;
+using UnityEngine;
+using static Notifications.PCNotiLib;
+
+namespace Notifications.NotificationStuff
+{
+    [HarmonyPatch(typeof(MonoBehaviourPunCallbacks), "OnPlayerLeftRoom")]
+    internal class LeavePatch : MonoBehaviour
+    {
+        private static void Prefix(Player otherPlayer)
+        {
+            if (otherPlayer != PhotonNetwork.LocalPlayer && otherPlayer != a)
+            {
+                SendNotification("<color=red>LEAVE</color>", $"{otherPlayer.NickName} Left the lobby.");
+                a = otherPlayer;
+            }
+        }
+
+        private static Player a;
+    }
+}
